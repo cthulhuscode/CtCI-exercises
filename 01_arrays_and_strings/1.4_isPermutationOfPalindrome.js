168dac5ef3b14031a3a9c115a29bccdd46f243d1
Enrique Ruvalcaba <jordison352@gmail.com>
2023-03-07 09:00:43 -0600

    added more solutions

--- 01_arrays_and_strings/1.4_isPermutationOfPalindrome.js
+++ 01_arrays_and_strings/1.4_isPermutationOfPalindrome.js
@@ -6,17 +6,22 @@
  * The palindrome does not need to be limited to just dictionary words.
  * EXAMPLE
  * Input: Tact Coa
- * Output: True (permutations: "taco cat", "atco eta", etc.)
- * Hints: #106, #121, #134, #136
+ * Output: True (permutations: "taco cat", "atco cta", etc.)
+ * Hints:
+ * - #106: you don't have to-and should not-generate all permutations.
+ * This would be very inefficient.
+ * - #121: What characteristics would a string that is a permutation of a palindrome have?
+ * - #134: Have you tried a hash table? You should be able to get this down to 0( N) time
+ * - #136: Can you reduce the space usage by using a bit vector?
  *
  */
 
 /**
- * 1. Using hashmap
+ * 1. Using a hashmap
  * Time: O(n)
  * Space: O(n)
  */
-function isPermutationOfPalindrome(str) {
+function isPermutationOfPalindrome1(str) {
   const eachCharFreq = new Map();
   let noSpacesStrLength = 0;
 
@@ -30,13 +35,20 @@ function isPermutationOfPalindrome(str) {
     }
   }
 
-  // If even
+  /** If even
+   * Strings with even length must have all even counts
+   * of characters
+   */
+
   if (noSpacesStrLength % 2 === 0) {
     for (let [key, count] of eachCharFreq) {
       if (count % 2 !== 0) return false;
     }
   } else {
-    // If odd
+    /** If odd
+     * Strings of an odd length must have exactly one
+     * character with a odd count.
+     */
     let isOdd = false;
     for (let [key, count] of eachCharFreq) {
       if (count % 2 === 1 && isOdd) return false;
@@ -47,4 +59,41 @@ function isPermutationOfPalindrome(str) {
   return true;
 }
 
-console.log(isPermutationOfPalindrome("tactcoapapa"));
+console.log(isPermutationOfPalindrome1("tactcoapapa"));
+
+/**
+ * 2. Cleaner version using a hashmap
+ * Time: O(n)
+ * Space: O(n)
+ */
+
+function isPermutationOfPalindrome2(str) {
+  const eachCharFreq = new Map();
+  let oddCount = 0;
+
+  /**
+   * The string can have no more than one character
+   * that is odd
+   */
+  for (let char of str) {
+    const charFreq = eachCharFreq.get(char);
+
+    if (char !== " ") {
+      if (charFreq) eachCharFreq.set(char, charFreq + 1);
+      else eachCharFreq.set(char, 1);
+
+      if (eachCharFreq.get(char) % 2 === 1) oddCount++;
+      else oddCount--;
+    }
+  }
+
+  return oddCount <= 1;
+}
+
+console.log(isPermutationOfPalindrome2("tactcoapapa"));
+
+/**
+ * 3. Using a bit vector
+ * Time: O(n)
+ * Space: O(1)
+ */
